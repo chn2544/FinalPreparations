e9132507cac8648c8b68d5296d0855ab06c2a841
Chinmay Gogte <107382292+chn2544@users.noreply.github.com>
2023-01-08 14:09:01 +0530

    Create JanPage.java
    Scenario for merge from Local Master to Cutted Branch

---
+++ src/main/java/com/qa/opencart/pages/JanPage.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class JanPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" Hiii I am From January Page....");
+	}
+
+}
