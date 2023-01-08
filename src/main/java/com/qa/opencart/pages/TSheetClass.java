e7d3377c264b4833df7626a06c20f91e516377c3
Gogte <Chinmay.Gogte@allscripts.com>
2023-01-08 13:32:09 +0530

    Added TSheetClass

---
+++ src/main/java/com/qa/opencart/pages/TSheetClass.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class TSheetClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" Hiii I am From T Sheets....");
+	}
+
+}
