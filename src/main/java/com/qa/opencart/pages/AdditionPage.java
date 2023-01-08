506ff632f4abc41e81164e8af0f2464806db8a38
Gogte <Chinmay.Gogte@allscripts.com>
2023-01-08 19:31:49 +0530

    Pushing code to repo

---
+++ src/main/java/com/qa/opencart/pages/AdditionPage.java
@@ -0,0 +1,15 @@
+package com.qa.opencart.pages;
+
+public class AdditionPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" Code for Stash Concept");
+		
+		//WIP 
+		int a=10,b=34,c=23;
+		System.out.println(a+b+c);
+	}
+
+}
