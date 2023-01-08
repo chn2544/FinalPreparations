d05852507cb1aa2684a26708e6398f0e633a5163
Gogte <Chinmay.Gogte@allscripts.com>
2023-01-08 12:55:09 +0530

    Pushing code for feature 1

---
+++ src/main/java/com/qa/opencart/pages/Feature1Page.java
@@ -0,0 +1,12 @@
+package com.qa.opencart.pages;
+
+public class Feature1Page {
+
+	String var1="Feature1";
+	int id=45235;
+	
+	public static void main(String ars[])
+	{
+		System.out.println("Feature F1");
+	}
+}
