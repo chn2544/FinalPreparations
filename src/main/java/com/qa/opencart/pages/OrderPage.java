a9462357baa3b68f89f3c5ee61323143ee60ef87
Gogte <Chinmay.Gogte@allscripts.com>
2023-01-08 14:36:08 +0530

    Pushing Code for OrderPage

---
+++ src/main/java/com/qa/opencart/pages/OrderPage.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class OrderPage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Order Id 228");
+	}
+
+}
