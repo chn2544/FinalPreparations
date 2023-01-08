1ccab3314d7df369eb05285099d4d2035d2f766c
Gogte <Chinmay.Gogte@allscripts.com>
2023-01-08 08:33:36 +0530

    Added CalenderPage

---
+++ src/main/java/com/qa/opencart/pages/CalnderPageClass.java
@@ -0,0 +1,11 @@
+package com.qa.opencart.pages;
+
+public class CalnderPageClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(" Hey Team, I have written CalenderPageClass Code on 8th Jan and Time is 8.30 AM");
+	}
+
+}
