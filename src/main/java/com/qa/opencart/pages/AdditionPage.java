68b3ee40e083f4f5c463c410c02953a7f6a75286
Gogte <Chinmay.Gogte@allscripts.com>
2023-01-08 19:42:26 +0530

    the Last Commit for Stash

--- src/main/java/com/qa/opencart/pages/AdditionPage.java
+++ src/main/java/com/qa/opencart/pages/AdditionPage.java
@@ -10,6 +10,11 @@ public class AdditionPage {
 		//WIP 
 		int a=10,b=34,c=23;
 		System.out.println(a+b+c);
+		
+		//To be Stashed
+		
+		int d=45;
+		System.out.println(d);
 
 		System.out.println("Happy New Year 2023");
 
